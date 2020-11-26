ade4c045fbe6d4a39e1f2015d274ee2ab18ce3f9
Agustin Carnevale <acarnevale@ecointeractive.com>
2020-11-26 01:09:41 -0300

    added different classnames prefix for prod

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,25 @@
+//domain: https://d2qa6fc2z9buq3.cloudfront.net
+
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+//avoid css/classNames colision between the different apps/microfrontends in Prod
+const generateClassname = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 const App = () => {
  return (
     <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div> 
+        <StylesProvider generateClassName={generateClassname}>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div> 
+        </StylesProvider>
     </BrowserRouter>
  );
 }
