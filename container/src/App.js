b8fb992e78cdd09f8203511bdaa9518e40806d8e
Agustin Carnevale <acarnevale@ecointeractive.com>
2020-11-29 01:45:47 -0300

    added auth, fixed history management, more webpack config

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,14 @@
 //deployed at: https://d2qa6fc2z9buq3.cloudfront.net
 
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Header from './components/Header';
+import ProgressBar from './components/ProgressBar';
+
+const MarketingLazy = lazy(()=> import('./components/MarketingApp'));
+const AuthLazy = lazy(()=> import('./components/AuthApp'));
 
 //avoid css/classNames colision between the different apps/microfrontends in Prod
 const generateClassname = createGenerateClassName({
@@ -17,7 +21,12 @@ const App = () => {
             <StylesProvider generateClassName={generateClassname}>
                 <div>
                     <Header />
-                    <MarketingApp />
+                    <Suspense fallback={<ProgressBar />}>
+                        <Switch>
+                            <Route path="/auth" component={AuthLazy} />
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
                 </div> 
             </StylesProvider>
         </BrowserRouter>
